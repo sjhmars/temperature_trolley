460d9aaa14b4815a41c34c2092b8107e3e280659
sjhmars <58546090+sjhmars@users.noreply.github.com>
2023-04-11 17:43:48 +0800

    配置端口

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
+<!--
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-04-11 16:31:45
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-04-11 17:40:32
+ * @FilePath: \temperature_trolley\src\views\AboutView.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>别叫</div>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-04-11 16:31:45
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-04-11 17:41:03
+ * @FilePath: \temperature_trolley\vue.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    open: true,           // 是否自动打开项目
+    host: '0.0.0.0',      // 制定域名
+    port: 8089,           // 端口号
+    hot: true,       // 热更新
+  }
 })
